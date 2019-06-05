2b71b534553a1fffe58c6c33a3165ddace6eaa13
sharkovadarya <sharkovadarya@gmail.com>
2019-06-05 18:28:15 +0300

    Updated in order to work with Docker.

--- Docker/src/main/kotlin/ru/hse/spb/kazakov/main.kt
+++ Docker/src/main/kotlin/ru/hse/spb/kazakov/main.kt
@@ -8,6 +8,7 @@ fun main(args: Array<String>) {
     System.setProperty("aws.accessKeyId", "accessKey")
     System.setProperty("aws.secretKey", "secretKey")
 
+    Thread.sleep(50)
     if (args.isEmpty()) {
         startQueues()
     } else if (args.size == 2) {
@@ -16,33 +17,47 @@ fun main(args: Array<String>) {
 }
 
 fun resendQueueMessages(prodQueueName: String, consQueueName: String) {
-    val sqs = AmazonSQSClientBuilder.standard()
-        .withEndpointConfiguration(
-            AwsClientBuilder.EndpointConfiguration("http://localhost:4576", "eu-west-1")
-        )
-        .build()
-    val prodQueueUrl = sqs.getQueueUrl(GetQueueUrlRequest(prodQueueName)).queueUrl
-    val consQueueUrl = sqs.getQueueUrl(GetQueueUrlRequest(consQueueName)).queueUrl
-
     while (true) {
-        sqs.receiveMessage(ReceiveMessageRequest(prodQueueUrl)).messages.forEach {
-            val num = it.body.toInt()
-            println(num)
-            sqs.sendMessage(SendMessageRequest(consQueueUrl, (num + 1).toString()))
-            sqs.deleteMessage(DeleteMessageRequest(prodQueueUrl, it.receiptHandle))
+        try {
+            val sqs = AmazonSQSClientBuilder.standard()
+                .withEndpointConfiguration(
+                    AwsClientBuilder.EndpointConfiguration("http://localhost:4576", "eu-west-1")
+                )
+                .build()
+            val prodQueueUrl = sqs.getQueueUrl(GetQueueUrlRequest(prodQueueName)).queueUrl
+            val consQueueUrl = sqs.getQueueUrl(GetQueueUrlRequest(consQueueName)).queueUrl
+
+            while (true) {
+                sqs.receiveMessage(ReceiveMessageRequest(prodQueueUrl)).messages.forEach {
+                    val num = it.body.toInt()
+                    println(num)
+                    sqs.deleteMessage(DeleteMessageRequest(prodQueueUrl, it.receiptHandle))
+                    sqs.sendMessage(SendMessageRequest(consQueueUrl, (num + 1).toString()))
+                }
+            }
+        } catch (e: Exception) {
+            println("waiting for localstack to start")
+            Thread.sleep(1)
         }
     }
 }
 
 fun startQueues() {
-    val sqs = AmazonSQSClientBuilder.standard()
-        .withEndpointConfiguration(
-            AwsClientBuilder.EndpointConfiguration("http://localhost:4576", "eu-west-1")
-        )
-        .build()
-    val createQueueRequestA = CreateQueueRequest("A")
-    val createQueueRequestB = CreateQueueRequest("B")
-    val queueUrl = sqs.createQueue(createQueueRequestA).queueUrl
-    sqs.createQueue(createQueueRequestB)
-    sqs.sendMessage(SendMessageRequest(queueUrl, "1"))
+    while (true) {
+        try {
+            val sqs = AmazonSQSClientBuilder.standard()
+                .withEndpointConfiguration(
+                    AwsClientBuilder.EndpointConfiguration("http://localhost:4576", "eu-west-1")
+                )
+                .build()
+            val createQueueRequestA = CreateQueueRequest("A")
+            val createQueueRequestB = CreateQueueRequest("B")
+            val queueUrl = sqs.createQueue(createQueueRequestA).queueUrl
+            sqs.createQueue(createQueueRequestB)
+            sqs.sendMessage(SendMessageRequest(queueUrl, "1"))
+        } catch (e: Exception) {
+            println("Waiting for Localstack services to start.")
+            Thread.sleep(1)
+        }
+    }
 }
