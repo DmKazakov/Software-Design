a99485f29e99b61da2b0234edbd8d430b7939b2f
sharkovadarya <sharkovadarya@gmail.com>
2019-06-05 16:37:01 +0300

    Added properties for credentials and partially fixed multiplied output error.

--- Docker/src/main/kotlin/ru/hse/spb/kazakov/main.kt
+++ Docker/src/main/kotlin/ru/hse/spb/kazakov/main.kt
@@ -5,6 +5,9 @@ import com.amazonaws.services.sqs.AmazonSQSClientBuilder
 import com.amazonaws.services.sqs.model.*
 
 fun main(args: Array<String>) {
+    System.setProperty("aws.accessKeyId", "accessKey")
+    System.setProperty("aws.secretKey", "secretKey")
+
     if (args.isEmpty()) {
         startQueues()
     } else if (args.size == 2) {
@@ -14,7 +17,9 @@ fun main(args: Array<String>) {
 
 fun resendQueueMessages(prodQueueName: String, consQueueName: String) {
     val sqs = AmazonSQSClientBuilder.standard()
-        .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration("http://localhost:4576", "eu-west-1"))
+        .withEndpointConfiguration(
+            AwsClientBuilder.EndpointConfiguration("http://localhost:4576", "eu-west-1")
+        )
         .build()
     val prodQueueUrl = sqs.getQueueUrl(GetQueueUrlRequest(prodQueueName)).queueUrl
     val consQueueUrl = sqs.getQueueUrl(GetQueueUrlRequest(consQueueName)).queueUrl
@@ -24,14 +29,16 @@ fun resendQueueMessages(prodQueueName: String, consQueueName: String) {
             val num = it.body.toInt()
             println(num)
             sqs.sendMessage(SendMessageRequest(consQueueUrl, (num + 1).toString()))
-            sqs.deleteMessage(DeleteMessageRequest(consQueueUrl, it.receiptHandle))
+            sqs.deleteMessage(DeleteMessageRequest(prodQueueUrl, it.receiptHandle))
         }
     }
 }
 
 fun startQueues() {
     val sqs = AmazonSQSClientBuilder.standard()
-        .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration("http://localhost:4576", "eu-west-1"))
+        .withEndpointConfiguration(
+            AwsClientBuilder.EndpointConfiguration("http://localhost:4576", "eu-west-1")
+        )
         .build()
     val createQueueRequestA = CreateQueueRequest("A")
     val createQueueRequestB = CreateQueueRequest("B")
